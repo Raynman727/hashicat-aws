0bd41c8f195e4cefa657bbbecac3222a959e5aed
Ray Dermon <ray.dermon@gmail.com>
2022-07-13 18:51:38 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "https://github.com/Raynman727/terraform-aws-s3-bucket"
+  source = "Raynman727/terraform-aws-s3-bucket"
 
   bucket = "my-s3-bucket"
   bucket_prefix = "${var.prefix}"
