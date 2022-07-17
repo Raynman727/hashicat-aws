f92978c75d9ace8da73ff8187153c1d36ad0a6ea
Ray Dermon <56550248+Raynman727@users.noreply.github.com>
2022-07-17 12:56:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "hashicat" {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    cidr_blocks     = ["74.0.0.0/0"]
+    cidr_blocks     = ["0.0.0.0/0"]
     prefix_list_ids = []
   }
 
